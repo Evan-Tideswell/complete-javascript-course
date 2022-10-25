7902790c778d8808c73a6f0ca2c518a7081b74ab
Evan Tideswell <f3nrirx@protonmail.com>
2022-10-25 15:49:48 +0100

    Completed video 12 on the course

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,42 @@
+/*let js = "amazing";
+console.log(40 + 80 + 23 - 10);
+
+console.log("Evan");
+console.log(23);
+
+let firstName = "Evan";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let evan_tideswell = "ET";
+let $function = 27;
+
+let people = "evan";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Engineer";
+
+let job1 = "programmer";
+let job2 = "engineer";
+
+console.log(myFirstJob);
+*/
+
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Evan");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
