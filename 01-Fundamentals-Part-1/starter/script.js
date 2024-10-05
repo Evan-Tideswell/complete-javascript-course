3109c4deeb0e95eb2e9da5d4ebce833b63666ba5
Evan Tideswell <f3nrirx@protonmail.com>
2024-10-05 12:16:59 +0100

    Completed E21 about Truthy Faslse Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -155,21 +155,43 @@ console.log(lastName);
 
 // Type Conversion and Coercion
 
-const inputYear = "1991";
-console.log(Number(inputYear), inputYear);
-console.log(Number(inputYear) + 18);
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
 
-console.log(Number("Evan"));
-console.log(typeof NaN);
+// console.log(Number("Evan"));
+// console.log(typeof NaN);
 
-console.log(String(23), 23);
+// console.log(String(23), 23);
 
 // type coercion
 
-console.log("I am " + 36 + " years old");
-console.log("23" - "10" - 3);
-console.log("23" / "2");
+// console.log("I am " + 36 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
 
-let n = "1" + 1; // '11'
-n = n - 1; // converts to number
-console.log(n);
+// let n = "1" + 1; // '11'
+// n = n - 1; // converts to number
+// console.log(n);
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Evan"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
