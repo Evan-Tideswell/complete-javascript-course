d5d18a9c672144f3c254d6c829c18c1ee584f0f3
Evan Tideswell <f3nrirx@protonmail.com>
2024-10-05 12:04:06 +0100

    Completed e20 on type conversion and coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -153,8 +153,23 @@ console.log(lastName);
 //   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 // }
 
-// Type Conversaion and Coercion
+// Type Conversion and Coercion
 
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
-console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Evan"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+
+console.log("I am " + 36 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; // '11'
+n = n - 1; // converts to number
+console.log(n);
